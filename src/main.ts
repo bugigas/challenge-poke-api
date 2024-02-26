83d088303c92a28f5c0230632472ddf22295766c
bugigas <tomikbudik@gmail.com>
2024-02-26 22:12:58 +0100

    Added swagger
    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Pokémon API')
+      .setDescription('API pro prohlížení a správu katalogu Pokémonů')
+      .setVersion('1.0')
+      .addTag('pokemons')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
